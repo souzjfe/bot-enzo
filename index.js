f5271e6176a014bea261d8c39b9fb34151428965
jefer <jefer@pop-os.localdomain>
2020-10-30 16:06:55 -0300

    Update token

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const token = "NzcxODAyMTE4NDM2MjkwNTcx.X5xavA.1q1E48C-PKK3FqyCRO1lMv6Ddsw"
+const token = 'NzcxODAyMTE4NDM2MjkwNTcx.X5xavA.BiwN5zvKJV5UAonYpwYTBlCrfUk'
+client.login(token);
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
 });
 
-client.on('message', msg => {
-    if (msg.content === 'ping') {
-        msg.reply('Pong!');
-    }
-});
 
-client.login(token);
+
+
+// client.on('message', msg => {
+//     if (msg.content === 'ping') {
+//         msg.reply('Pong!');
+//     }
+// });

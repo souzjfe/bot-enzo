bf52b3b875a25ff0e4769e640df69d62042fb573
jefer <jefer@pop-os.localdomain>
2020-10-30 18:30:34 -0300

    Sending message in tts

--- index.js
+++ index.js
@@ -1,16 +1,29 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const token = 'NzcxODAyMTE4NDM2MjkwNTcx.X5xavA.BiwN5zvKJV5UAonYpwYTBlCrfUk'
+const xingamentos = require('./src/falas.js');
+const reacoes = require('./src/falas.js');
+const desmotivacional = require('./src/falas.js');
+const token = ''
 client.login(token);
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
 });
 
-
-
-
-// client.on('message', msg => {
-//     if (msg.content === 'ping') {
-//         msg.reply('Pong!');
-//     }
-// });
+client.on('message', async message => {
+    if (message.content == "=x") {
+        (await message.channel.send(xingamentos[Math.floor((xingamentos.length) * Math.random())].msgtts, {
+            tts: true
+        }));
+        console.log(xingamentos)
+    } else if (message.content == "=r") {
+        (await message.channel.send(reacoes[Math.floor((reacoes.length) * Math.random())].msgtts, {
+            tts: true
+        }));
+        console.log(reacoes)
+    } else if (message.content == "=d") {
+        (await message.channel.send(desmotivacional[Math.floor((desmotivacional.length) * Math.random())].msgtts, {
+            tts: true
+        }));
+        console.log(desmotivacional)
+    }
+});
---
+++ src/falas.js
@@ -0,0 +1,138 @@
+module.exports = xingamentos = [{
+    msg: "\nvenha\nsuaputa\nEnzo",
+    msgtts: "Venha, sua puta",
+    audio: "",
+}, {
+    msg: "\num completo corno mesmo\nEnzo",
+    msgtts: "Um completo corno mesmo",
+    audio: " ",
+}, {
+    msg: "\nwtf viccari vc tem merda na cabeça mesmo\nEnzo",
+    msgtts: "WTF Viccari, vc tem merda na cabeça mesmo",
+    audio: " ",
+}, {
+    msg: "\nclb mano\nta suave\nEnzo",
+    msgtts: "Cala a boca mano, tá suave",
+    audio: "",
+}, {
+    msg: "\nvai la corno\nta na hora de mamar\nEnzo",
+    msgtts: "Vai la corno, tá na hora de mamar",
+    audio: "",
+}, {
+    msg: "\nvsf escrot\nEnzo",
+    msgtts: "Vai se foder, escroto",
+    audio: "",
+}, {
+    msg: "\nso nego da 5a seriie\nEnzo",
+    msgtts: "Só nego da quinta sério",
+    audio: "",
+}, {
+    msg: "\nverme\nEnzo",
+    msgtts: "Verme",
+    audio: "",
+}, {
+    msg: "\ntnc\nEnzo",
+    msgtts: "Tomar no cu",
+    audio: "",
+}, ]
+
+module.exports = reclamacoes = [{
+    msg: "\npare, chega dissoooo\nEnzo",
+    msgtts: "Pare, chega disso",
+    audio: " ",
+}, {
+    msg: "\nah nao man n comeca\nEnzo",
+    msgtts: "Ah, não mano, não começa",
+    audio: "",
+}, {
+    msg: "\npo n da pra fazer nada essa procaria de grupo\nque ja começam a me criticar\nsendo q n faço nada demais\nEnzo",
+    msgtts: "Poxa, não dá para fazer nada nessa porcaria de grupo que já começam a me criticar, sendo que não faço nada demais",
+    audio: "",
+}, {
+    msg: "\nficou me enchendo o saco pra joga\nEnzo",
+    msgtts: "Ficou me enchendo o saco para jogar",
+    audio: "",
+}, {
+    msg: "\numas 2 partidas segfuidas dando spam\nEnzo",
+    msgtts: "Umas 2 partidas seguidas dando spam",
+    audio: "",
+}, {
+    msg: "\nso ver o nick do cara\nmds\nmano\nEnzo",
+    msgtts: "Só ver o nick do cara, meu Deus, mano",
+    audio: "",
+}, {
+    msg: "\nme deixa em paz vai\nna mora\n8na moral\nso me esquece\nEnzo",
+    msgtts: "Me deixa em paz vai, na moral, só me esquece",
+    audio: "",
+}, ]
+
+module.exports = desmotivacional = [{
+    msg: "\ndesisto disso ae flw\nEnzo",
+    msgtts: "Desisto disso ae falou",
+    audio: "",
+}, {
+    msg: "\nfds\nodeio \nEnzo",
+    msgtts: "Foda-se, odeio",
+    audio: "",
+}, {
+    msg: "\nEh demais ja\nEnzo",
+    msgtts: "É demais já",
+    audio: "",
+}, {
+    msg: "\nkkkkkkk reantinha ja eh passado meu amigo\nEnzo",
+    msgtts: "KKKKKK Renatinha já é passado, meu amigo",
+    audio: "",
+}, {
+
+
+}]
+
+module.exports = reacoes = [{
+    msg: "\nn para vei\nna boa\nn aguento mais\nEnzo",
+    msgtts: "Não, para véi, na boa, não aguento mais",
+    audio: "",
+}, {
+    msg: "\nele esta brilhando pq encontrou um novo caminho para viver\nEnzo",
+    msgtts: "Ele está brilhando porque encontrou um novo caminho para viver",
+    audio: "",
+}, {
+    msg: "\newita\nEnzo",
+    msgtts: "Eita",
+    audio: "",
+}, {
+    msg: "\ncaraio\nEnzo",
+    msgtts: "Caralho",
+    audio: "",
+}, {
+    msg: "\nodeio vcs\nEnzo",
+    msgtts: "Odeio vocês",
+    audio: "",
+}, {
+    msg: "\nnossa\nEnzo",
+    msgtts: "Nossa",
+    audio: "",
+}, {
+    msg: "\nq??\nmds cara\nEnzo",
+    msgtts: "Que? Meu Deus cara",
+    audio: "",
+}, {
+    msg: "\nvcs q ficam fazendo meme de tudo cara\nq odeio\nEnzo",
+    msgtts: "Vocês que ficam fazendo meme de tudo cara, odeio",
+    audio: "",
+}, {
+    msg: "\nah sim\nclaro\nso ver o seu chat la no lol",
+    msgtts: "Ah sim, claro, só ver o seu chat la no LOL",
+    audio: "",
+}, {
+    msg: "\nblz\nEnzo",
+    msgtts: "Beleza",
+    audio: "",
+}, {
+    msg: "\nEnzo saiu\nEnzo",
+    msgtts: "Enzo saiu",
+    audio: "",
+}, {
+    msg: "\nJeferson adicionou Enzo\nEnzo",
+    msgtts: "Jeferson adicionou Enzo",
+    audio: "",
+}, ]
